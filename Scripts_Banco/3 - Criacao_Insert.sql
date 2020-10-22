999d306796ae50b3221326e449487059a0e6a115
Andre Souza <andrecsi.souza@gmail.com>
2020-10-21 21:59:58 -0300

    Adição dos scripts de banco de dados

---
+++ Scripts_Banco/3 - Criacao_Insert.sql
@@ -0,0 +1,11 @@
+USE [BD_CHALENGE]
+GO
+
+INSERT INTO [dbo].[TB_CURTIDAS]
+           ([IP_CURTIDA]
+           ,[QTDE_CURTIDA]
+           ,[BROWNSER])
+     VALUES
+           ('192.168.1.1',90,'')
+GO
+
